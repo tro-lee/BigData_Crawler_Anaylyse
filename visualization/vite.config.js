3fff0ed1fbd3794da62ef640dc5f3e56ff34bf3b
tro-lee <1481895142@qq.com>
2024-04-16 12:05:20 +0800

    分成模块

--- visualization/vite.config.js
+++ visualization/vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
+const currentDir = path.dirname(import.meta.url)
+
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@data': currentDir+ '/../result/result.json'
+    }
+  }
 })
