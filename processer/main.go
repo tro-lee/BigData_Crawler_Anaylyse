c20c3fbedfcdef735635d64514eaa2a26afee46f
tro-lee <1481895142@qq.com>
2024-04-18 11:10:55 +0800

    best

--- processer/main.go
+++ processer/main.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"log"
 	"regexp"
+	"strings"
+	"unicode/utf8"
 
 	"github.com/go-ego/gse"
 )
@@ -40,9 +42,39 @@ func main() {
 		log.Fatalln(err)
 	}
 
+	successChan := make(chan int)
+
+	fmt.Println("开始加载词典")
+
 	// 开始分词
 	loadDict()
 
+	fmt.Println("开始加工")
+
+	// 提取关键字
+	go segWord(clearNews, successChan)
+
+	// 提取国家关键字
+	go countryResult(clearNews, successChan)
+
+	// 提取国家词汇
+	go countryResultPro(clearNews, successChan)
+
+	successedNum := 0
+	for {
+		if successedNum >= 3 {
+			break
+		}
+
+		num := <-successChan
+		fmt.Println(num)
+		successedNum++
+	}
+	fmt.Println("完成加工")
+}
+
+// 得到分词文件
+func segWord(clearNews []ClearNews, successChan chan int) {
 	titleResult := make(map[string]int, len(clearNews))
 	contentResult := make(map[string]int, len(clearNews))
 
@@ -51,6 +83,11 @@ func main() {
 
 		for _, word := range words {
 			reg := regexp.MustCompile("^[0-9a-zA-Z[:space:]，：（）《》〈〉“；—、`”.,;_+='？！]+$")
+
+			if utf8.RuneCountInString(word) <= 1 {
+				continue
+			}
+
 			if !reg.MatchString(word) {
 				titleResult[word]++
 			}
@@ -59,6 +96,10 @@ func main() {
 		contents := segCut(clearNews[i].Summary)
 		for _, content := range contents {
 			reg := regexp.MustCompile("^[0-9a-zA-Z[:space:]，：（）《》〈〉“；—、`”.,;_+='？！]+$")
+
+			if utf8.RuneCountInString(content) <= 1 {
+				continue
+			}
 
 			if !reg.MatchString(content) {
 				contentResult[content]++
@@ -74,7 +115,11 @@ func main() {
 	JsonToFile(contentJson, "./result/seg_content_result.json")
 	fmt.Println("新闻内容分词完成")
 
-	// 提取国家关键字
+	successChan <- 1
+}
+
+// 得到关键字提取
+func countryResult(clearNews []ClearNews, successChan chan int) {
 	countryRegex := getCountryRegex()
 	countryResult := make(map[string]int, len(clearNews))
 
@@ -88,6 +133,13 @@ func main() {
 	JsonToFile(countryJson, "./result/country_result.json")
 	fmt.Println("国家关键字提取完成")
 
+	successChan <- 2
+}
+
+// 提取词汇
+func countryResultPro(clearNews []ClearNews, successChan chan int) {
+	countryRegex := getCountryRegex()
+
 	// 提取国家词汇
 	countryResultPro := make(map[string]map[string]int, len(clearNews))
 	for _, value := range clearNews {
@@ -99,6 +151,13 @@ func main() {
 
 			words := segCut(value.Title)
 			for _, word := range words {
+				if strings.EqualFold(word, string(title)) {
+					continue
+				}
+				if utf8.RuneCountInString(word) <= 1 {
+					continue
+				}
+
 				reg := regexp.MustCompile("^[0-9a-zA-Z[:space:]，：（）《》〈〉“；—、`”.,;_+='？！]+$")
 				if !reg.MatchString(word) {
 					countryResultPro[string(title)][word]++
@@ -106,6 +165,7 @@ func main() {
 			}
 		}
 	}
+
 	countryJsonPro, _ := json.Marshal(countryResultPro)
 	JsonToFile(countryJsonPro, "./result/country_result_pro.json")
 	fmt.Println("国家词汇提取完成")
@@ -121,6 +181,9 @@ func main() {
 			if countryRegex.MatchString(k) {
 				continue
 			}
+			if utf8.RuneCountInString(k) <= 1 {
+				continue
+			}
 
 			if v > max {
 				max = v
@@ -131,6 +194,9 @@ func main() {
 	}
 	countryMaxJson, _ := json.Marshal(countryMaxResult)
 	JsonToFile(countryMaxJson, "./result/country_max_result.json")
+	fmt.Println("国家词汇Pro提取完成")
+
+	successChan <- 3
 }
 
 func GetClearData(news []News) ([]ClearNews, error) {
--- visualization/App.jsx
+++ visualization/App.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react'
 import CountryCharts from './modules/CountryCharts'
+import CountryChartsPro from './modules/CountryChartsPro'
 import SegCharts from './modules/SegCharts'
 import SegChartsPro from './modules/SegChartsPro'
+
 function App() {
-  const [chart, setChart] = useState(3)
+  const [chart, setChart] = useState(4)
   if (chart === 1) {
     return (
       <>
@@ -23,8 +25,16 @@ function App() {
   else if (chart === 3) {
     return (
       <>
-        <button onClick={() => setChart(1)}>Next Chart</button>
+        <button onClick={() => setChart(4)}>Next Chart</button>
         <CountryCharts></CountryCharts>
+      </>
+    )
+  }
+  else if (chart === 4) {
+    return (
+      <>
+        <button onClick={() => setChart(1)}>Next Chart</button>
+        <CountryChartsPro></CountryChartsPro>
       </>
     )
   }
